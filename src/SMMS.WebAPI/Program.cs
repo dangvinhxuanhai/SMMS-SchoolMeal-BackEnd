f68f665bb41e2c0cea47cfe0a02412af78511ab1
dangvinhxuanhai@gmail.com <dangvinhxuanhai@gmail.com>
2025-10-16 18:05:17 +0700

    update mediator and cqrs

--- src/SMMS.WebAPI/Program.cs
+++ src/SMMS.WebAPI/Program.cs
@@ -1,7 +1,11 @@
+using FluentValidation;
+using MediatR;
 using Microsoft.EntityFrameworkCore;
+using SMMS.Application.Common.Validators;
+using SMMS.Application.Features.foodmenu.Handlers;
 using SMMS.Application.Features.foodmenu.Interfaces;
-using SMMS.Application.Features.foodmenu.Service;
 using SMMS.Persistence.DbContextSite;
+using SMMS.Persistence.Repositories.foodmenu;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,7 +13,13 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddScoped<IWeeklyMenuService, WeeklyMenuService>();
+builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(SMMS.Application.Features.foodmenu.Queries.GetWeekMenuQuery).Assembly));
+// FluentValidation: quét toàn bộ validators trong Application
+builder.Services.AddValidatorsFromAssembly(typeof(WeeklyMenuHandler).Assembly);
+// Pipeline: chạy validation trước handler
+builder.Services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehavior<,>));
+
+builder.Services.AddScoped<IWeeklyMenuRepository, WeeklyMenuRepository>();
 builder.Services.AddDbContext<EduMealContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("SqlConnection")));
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
